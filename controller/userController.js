2f555082c515449f9f5c4a70435ba08f239597d7
Priyanka Mary Chrstine <marycpriyanka@gmail.com>
2022-04-18 16:58:05 -0700

    Add Api route to post a user

--- controller/userController.js
+++ controller/userController.js
@@ -4,9 +4,19 @@ module.exports = {
     // Get all users
     getUsers(req, res) {
         User.find()
-            .then((users) => res.status(200).json(users))
-            .catch((err) => res.status(500).json(err));
+            .then(users => res.status(200).json(users))
+            .catch(err => res.status(500).json(err));
     },
 
-    
+    // Post a new user
+    createUser(req, res) {
+        if (req.body.username && req.body.email) {
+            User.create(req.body)
+            .then(user => res.status(200).json(user))
+            .catch(err => res.status(500).json(err));
+        }
+        else {
+            res.status(404).json("Request body must contain username and password");
+        }
+    }
 }
